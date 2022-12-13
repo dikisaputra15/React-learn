1e5b993dcb9756c790c45a1a9e8219db24986333
dikisaputra15 <dikimistak@gmail.com>
2022-12-13 17:02:19 +0700

    until routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Learn from './components/Hello'
-import Learning from './components/Learning'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Learning />
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  render(){
+    return(
+      <div className="App">
+        <div>
+
+        <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Easy Learning</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">About Us</Nav.Link>
+            <Nav.Link href="#action2">Contact Us</Nav.Link>
+            <Nav.Link href="#action2">Blog</Nav.Link>
+            <Nav.Link href="#action2">Profile</Nav.Link>
+           
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+          <Breadcrumb>
+          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+          Library
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Data</Breadcrumb.Item>
+          </Breadcrumb>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
